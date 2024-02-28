57acd5773b11bb78d8fc64eb87ddb188cacacac3
ethansjc <ethansjcheong@gmail.com>
2024-02-28 12:19:12 -0500

    Refactor utils

--- utils/index.ts
+++ utils/index.ts
@@ -4,9 +4,8 @@ export async function fetchCars(filters: FilterProps) {
     const { manufacturer, year, model, limit, fuel } = filters;
 
     const headers = {
-        'X-RapidAPI-Key': '9a8fa52667msh08d8380391a4614p1f3ad4jsnf0f46b54e703',
+        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || '',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-
     }
 
     const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, { headers: headers, });
@@ -36,7 +35,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
     const { make, year, model } = car;
     
-    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(' ')[0]);
     url.searchParams.append('zoomType', 'fullscreen');
